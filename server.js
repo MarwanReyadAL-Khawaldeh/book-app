4beb9b55f81c777ef6cbad0b706d3a0b90d6742a
MarwanReyadAL-Khawaldeh <marwanreyad@gmail.com>
2021-04-14 15:34:22 +0300

    Lab-12

--- server.js
+++ server.js
@@ -24,20 +24,27 @@ server.use(cors());
 
 server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
-server.get('/', getBook);
-server.post('/addBook', addBookHandler);
-server.get('/bookDetails/:bookID',getOneBookDetails);
+server.get('/',homePage);
+server.post('/addBookHandler', addBookHandler);
+server.get('/getOneBookDetails/:id', getOneBookDetails);
 
 server.set('view engine', 'ejs');
 
 
-// server.get('/', (req, res) => {
-//     res.render('pages/index');
-// });
+function homePage(req, res) {
+
+    let SQL = `Select * from books`;
+    client.query(SQL)
+        .then(result => {
+            console.log(result.rows);
+            res.render('pages/index', { book: result.rows });
+        });
+}
 
 server.get('/searches/new', (req, res) => {
     res.render('pages/searches/new');
 });
+
 
 server.post('/searches', (req, res) => {
     let search = req.body.search;
@@ -53,36 +60,25 @@ server.post('/searches', (req, res) => {
 
 function addBookHandler(req, res) {
     console.log(req.body);
-    let { title, author, description, image } = req.body;
-    let SQL = `INSERT INTO book_app (title,description,contact,image) VALUES ($1,$2,$3,$4) RETURNING *;`;
-    let safeValues = [title, author, description, image];
+    let { title, author,isbn, img , description } = req.body;
+    let SQL = `INSERT INTO books (title,author,isbn,img,description) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+    let safeValues = [title, author,isbn, img , description ];
     client.query(SQL, safeValues)
         .then(result => {
             console.log(result.rows);
 
-            res.redirect(`/bookDetails/${result.rows[0].id}`);
+            res.redirect(`/getOneBookDetails/${result.rows[0].id}`);
         });
 }
 
-function getBook(req, res) {
-    let SQL = `SELECT * FROM book_app;`;
-    client.query(SQL)
-        .then(results => {
-            res.render('pages/index', { book: results.rows });
-        })
-        .catch(err => {
-            res.render('error', { error: err });
-        });
-}
+
 function getOneBookDetails(req, res) {
-    // get from DB task id =3
     console.log(req.params);
-    let SQL = `SELECT * FROM book_app WHERE id=$1;`;
-    let safeValue = [req.params.bookID];
+    let SQL = `SELECT * FROM books WHERE id=$1;`;
+    let safeValue = [req.params.id];
     client.query(SQL, safeValue)
         .then(result => {
-            console.log(result.rows);
-            res.render('oneTask', { book: result.rows[0] });
+            res.render('pages/book/detail', { books: result.rows[0] });
         });
 }
 
@@ -90,8 +86,8 @@ function Book(data) {
     this.title = data.volumeInfo.title;
     this.author = data.volumeInfo.authors;
     this.description = data.volumeInfo.description;
-    this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
-
+    this.img = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+    this.isbn = (data.volumeInfo.industryIdentifiers)?data.volumeInfo.industryIdentifiers[0].identifier : 'ISBN N/A' ;
 }
 
 server.get('*', (req, res) => {
